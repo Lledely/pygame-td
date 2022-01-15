08aadfc9ea3a14cff0aad7f8fd5e0a32a8619ab8
Lledely <nemtsov.vovan@yandex.ru>
2022-01-15 22:48:28 +0300

    some stuff

--- towers.py
+++ towers.py
@@ -1,8 +1,9 @@
 class __Tower(object):
     
-    def __init__(self) -> _Tower:
-        self.damage = 1
-        self.attack_speed = 1 # in shoots per second
+    def __init__(self, damage=1, attack_speed=1, attack_range=1) -> _Tower:
+        self.damage = damage
+        self.attack_speed = attack_speed # in shoots per second
+        self.attack_range = attack_range
         
     def deploy(self, x=0, y=0) -> None:
         self.x = x
